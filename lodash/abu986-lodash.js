078108bc549c84eb50d2df68a58ce6eeff4249f5
unknown <b13053754647@163.com>
2020-12-08 20:40:05 +0800

    fir

--- lodash/abu986-lodash.js
+++ lodash/abu986-lodash.js
@@ -1,13 +1,55 @@
-var Abu986=function(){
-  function reduce(ary, reducer, result) {
-    var start = 0
-    if (result == undefined) {
-      result = ary[0]
-      start = 1
+var abu986=function(){
+  function chunk(array,size){
+    var res=[]
+    if(!array||size<1){
+      return []
     }
-    for (var i = start; i < ary.length; i++) {
-      result = reducer(result, ary[i], i, ary)
+    for(var i=0;i<array.length;i+=size){
+      res.push(array.slice(i,i+size))
     }
-    return result
+    return res
   }
-}
+  function compact(array){
+    var res=[]
+    for(var i=0;i<array.length;i++){
+      if(array[i]){
+        res.push(array[i])
+      }
+    }
+    return res
+  }
+  function join(array,separator){
+    var res=''
+    for(var i=0;i<array.length-1;i++){
+      res+=array[i]+''+separator
+    }
+    res=res+array[array.length-1]
+    return res
+  }
+  function last(array){
+    return res=array[array.length-1]
+  }
+  function lastIndexOf(array, value,fromIndex=array.length-1){
+    for(var i=fromIndex;i>=0;i--){
+      if(array[i]==value){
+        return i
+      }
+    }
+    return -1
+  }
+  function drop(array,n){
+    if(!n){
+      n=1
+    }
+    if(n>=array.length){
+      return []
+    }var res=[]
+    for(var i=n;i<array.length;i++){
+      res.push(array[i])
+    }
+    return res
+  }
+  function dropRightWhile(array,predicate){
+
+  }
+}()
