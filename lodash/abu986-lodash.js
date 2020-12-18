cee60461ab9a871334d355cbf47eeb369abf1915
unknown <b13053754647@163.com>
2020-12-18 16:52:17 +0800

    fsd

--- lodash/abu986-lodash.js
+++ lodash/abu986-lodash.js
@@ -18,15 +18,26 @@ var abu986 = (function () {
     }
     return res
   }
-  function concat(ary, ...args) {
-    var res = [...ary];
-    for (var i = 0; i < args.length; i++) {
+  function concat(array, ...args) {
+    let res = [...array];
+    for (let i = 0; i < args.length; i++) {
         if (Array.isArray(args[i])) {
             res.push(...args[i]);
         } else {
             res.push(args[i]);
         }
     }
+    return res
+  }
+  function differernce(arr,val){
+    var res=[]
+    var r=concat([],...val)
+    for(var i=0;i<arr.length;i++){
+      if(r.indexOf(arr[i])==-1){
+        res.push(arr[i])
+      }
+    }
+    return res
   }
   function join(array,separator){
     var res=''
@@ -58,85 +69,123 @@ var abu986 = (function () {
     if(n>=array.length){
       return []
     }
-    array.length=array.length-n
-    return array
+    return array.slice(0,array.length-n)
   }
   function dropRightWhile(array,predicate){
    
   }
-  function fill(array,value,start=0,end=array.length){
+  function fill(array,value,start,end){
+    var a=end-start
+    if(a>0){
       for(var i=start;i<end;i++){
         array[i]=value
       }
+    }
     return array
   }
- 
+  function findIndex(array,predicate,fromIndex){
+
+  }
+  function findLastIndex(array,predicate,fromIndex){
+
+  }
   function flatten(array){
-    var res=[]
-    for(var i=0;i<array.length;i++){
-      if(Array.isArray(array[i])){
-        res.push(...array[i])
-      }else{
-        res.push(array[i])
-      }
+    var res = [];
+    for (var i = 0; i < array.length; i++) {
+        if (Array.isArray(array[i])) {
+            res.push(...array[i]);
+        } else {
+            res.push(array[i]);
+        }
     }
     return res
   }
   function flattenDeep(array){
     var res=[]
     for(var i=0;i<array.length;i++){
-      var item=array[i]
-      if(Array.isArray(item)){
-        res.push(...flattenDeep(item))
-      }res.push(item)
+      if(Array.isArray(array[i])){
+        res = res.concat(flattenDeep(array[i]))
+      }else{
+        res.push(array[i])
+      }
     }
     return res
   }
   function flattenDepth(array,depth){
-    for(var i=0;i<depth;i++){
-      flatten(array)
+    var res=array
+    while(depth){
+      res=flatten(array)
+      depth--
     }
-    return array
+    return res
     /*var res=[]
     for(var i=0;i<array.length;i++){
-      var item=array[i]
       if(Array.isArray(item&&depth>=a)){
-        res.push(...flattenDepth(item))
-        }res.push(item)
+        res.push(...flattenDepth(array[i]))
+        }res.push(array[i])
       }
       return res*/
     } 
-    function fromPairs(pairs){
-      var res={}
-      for(var i=0;i<pairs.length;i++){
-        res[pairs[i][0]]=pairs[i][1]
+  function fromPairs(pairs){
+    var res={}
+    for(var i=0;i<pairs.length;i++){
+      res[pairs[i][0]]=pairs[i][1]
+    }
+    return res
+  }
+  function head(array){
+    if(!array){
+      return []
+    }
+    return array[0]
+  }
+  function indexOf(array,value,fromIndex=0){
+    for(var i=fromIndex;i<array.length;i++){
+      if(array[i]==value){
+        return i
       }
-      return res
     }
-    function head(array){
-      if(!array){
-        return []
+    return -1
+  }
+  function initial(arr){
+    return arr.slice(0,arr.length-1)
+  }
+  function nth(arr,val){
+    if(val<0){
+      var res=[]
+      var a=MAth.abs(val)
+      for(var i=arr.length-1;i>=0;i--){
+        res.push(arr[i])
       }
-      return array[0]
+      return res[a-1]
+    }else{
+      return arr[val]
     }
-    function indexOf(array,value,fromIndex=0){
-      for(var i=fromIndex;i<array.length;i++){
-        if(array[i]==value){
-          return i
-        }
+  }
+  function pull(arr,...val){
+    var res=[]
+    for(let key of arr){
+      if(!val.includes(key)){
+        res.push(key)
       }
-      return -1
     }
-    function findIndex(array,predicate,fromIndex){
-
+    return res
+  }
+  function pullAll(arr,val){
+    var res=[]
+    for(let key of arr){
+      if(!val.includes(key)){
+        res.push(key)
+      }
     }
-    function findLastIndex(array,predicate,fromIndex){
-
-    }
+    return res
+  }
+  
   return {
     chunk,
     compact,
     concat,
+    differernce,
     join,
     last,
     lastIndexOf,
@@ -149,5 +198,12 @@ var abu986 = (function () {
     flatten,
     flattenDeep,
     flattenDepth,
+    fromPairs,
+    head,
+    indexOf,
+    initial,
+    nth,
+    pull,
+    pullAll,
   };
 })();
